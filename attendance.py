e625c356161453ca5dbad3422bf7a7c0dcafeabe
Bryan Lai <bryan.j.lai@stonybrook.edu>
2023-05-10 12:23:47 -0400

    Updated active members May 10th, 2023

--- attendance.py
+++ attendance.py
@@ -37,7 +37,7 @@ def edit_csv_row(row_index, new_row_values):
         csv_writer = csv.writer(file)
         csv_writer.writerows(rows)
 
-def add_to_attendance_csv(new_file, event_name):
+def add_to_attendance_csv(new_file):
     #Some attendance forms may have first name, last name and email at different indices. 
     fn_idx, ln_idx, email_idx = index_of_col_names(new_file)
     with open(new_file, 'r') as source, open("attendancerecords.csv", 'a', newline='') as target:
@@ -52,25 +52,25 @@ def add_to_attendance_csv(new_file, event_name):
             row_two_vals = member_exists(row[email_idx])
             if(row_two_vals):
                 row_idx, row_to_be_updated = row_two_vals
-                #Add event name to the list of event_names
-                row_to_be_updated[3] += ", %s" % event_name
                 #Increase the # of events for the updated row by 1.
-                num_of_events = int(row_to_be_updated[4])
+                num_of_events = int(row_to_be_updated[3])
                 num_of_events += 1
-                row_to_be_updated[4] = str(num_of_events)
+                row_to_be_updated[3] = str(num_of_events)
                 #Check to see if the # of events attended is greater than 2 - if so, then they are an active member. 
                 if(num_of_events >= 2):
-                    row_to_be_updated[5] = "Yes"
+                    row_to_be_updated[4] = "Yes"
                 edit_csv_row(row_idx, row_to_be_updated)
             #Add new entry to attendance.
             else:
-                new_entry = [row[fn_idx], row[ln_idx], row[email_idx], event_name, 1, "No"]
+                new_entry = [row[fn_idx], row[ln_idx], row[email_idx], 1, "No"]
                 attendance_writer.writerow(new_entry)
 
 args = sys.argv
-if(len(args) < 3):
-    print("Correct Usage: python attendance.py <csv-file> <name-of-event>")
+if(len(args) < 2):
+    print("Correct Usage: python attendance.py <csv-file> ")
 else:
-    add_to_attendance_csv(args[1], args[2])
+    event_name = args[1][7:-4]
+    add_to_attendance_csv(args[1])
+    print("Completed attendance submission of: " + event_name)
 
 
